5d578dfaa9defd9f70eead988287b2346ad63f07
MaryEtak <marykatewhite@gmail.com>
2020-10-28 14:50:35 -0400

    commenting out degree selection stuff for now

--- public/javascript.js
+++ public/javascript.js
@@ -4,27 +4,15 @@ var addressValidator = require("address-validator");
 var _ = require("underscore");
 
 function validateForm() {
-  var degree = document.forms["contactInputs"]["degreeType"].value;
-  var field = document.forms["contactInputs"]["studyField"].value;
+  // var degreeList = document.forms["contactInputs"]["degreeType"];
+  // var fieldList = document.forms["contactInputs"]["studyField"];
+  // var degree = degreeList.options[degreeList.selectedIndex].text;
+  // var field = fieldList.options[fieldList.selectedIndex].text;
   var firstName = document.forms["contactInputs"]["first_name"].value;
   var lastName = document.forms["contactInputs"]["last_name"].value;
   var email = document.forms["contactInputs"]["email"].value;
   var phone = document.forms["contactInputs"]["phone"].value;
   var address = document.forms["contactInputs"]["address"].value;
-
-  if (degree == "") {
-    alert("Please select a degree type.");
-    return false;
-  } else {
-    console.log(degree);
-  }
-
-  if (field == "") {
-    alert("Please select a field of study.");
-    return false;
-  } else {
-    console.log(field);
-  }
 
   if (firstName == "") {
     alert("Please enter a first name.");
